9938c6a8bacfbb57f9372f284b4ddfa61f4ac962
Martin Nikolov <martin.nikolov@abv.bg>
2012-03-12 10:23:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mhp::Application.config.secret_token = '867feefd600463b274d9e229da46a524a228a7366c0115a306d79f8fa4b89e0e455f5d91d367171a705d2e61977716adfa2849ac17de74d3fcf6573e1d14de3c'
